d50f8c1a5c38712b3f275054e1756f66b5245cd8
lmatthew87 <81183261+lmatthew87@users.noreply.github.com>
2021-07-29 12:39:50 -0700

    validation
    Securing the app

--- index.js
+++ index.js
@@ -4,6 +4,40 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
+const cors = require('cors');
+app.use(cors());
+
+app.post('/users', (req, res) => {
+  let hashedPassword = Users.hashPassword(req.body.Password);
+  Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+    .then((user) => {
+      if (user) {
+      //If the user is found, send a response that it already exists
+        return res.status(400).send(req.body.Username + ' already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) => { res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
+
+const { check, validationResult } = require('express-validator');
+check('Username', 'Username contains non-alphanumeric characters - not allowed.').isAlphanumeric()
+
 mongoose.connect('mongodb://localhost:27017/boxoffice', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express');
@@ -36,8 +70,9 @@ app.get('/secreturl', (req, res) => {
 
 });
 
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
 const express = require('express'),
 morgan = require('morgan');
